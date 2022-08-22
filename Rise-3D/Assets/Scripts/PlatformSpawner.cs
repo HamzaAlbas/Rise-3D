685ad1cf83a27ea155d935f23c977c5fcfc4f283
Hamza Albaş <hamza.albas@hotmail.com>
2022-08-22 14:08:22 +0300

    Platform Spawner

---
+++ Rise-3D/Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject platformPrefab;
+
+    private void Start()
+    {
+        StartCoroutine(CubeSpawnCoroutine());
+    }
+
+    IEnumerator CubeSpawnCoroutine()
+    {
+        while (true)
+        {
+            SpawnCube();
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    public void SpawnCube()
+    {
+        Vector3 position = new Vector3(Random.Range(-2f, 2f), 0, 0);
+        Instantiate(platformPrefab, position, Quaternion.identity);   
+    }
+}
