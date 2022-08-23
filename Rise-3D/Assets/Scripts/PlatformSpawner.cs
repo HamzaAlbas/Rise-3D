505c351291e1e6e2ff5a2d238ba56484d4fda9e4
Hamza Albaş <hamza.albas@hotmail.com>
2022-08-23 12:27:49 +0300

    Platform spawner depending on the screen size

--- Rise-3D/Assets/Scripts/PlatformSpawner.cs
+++ Rise-3D/Assets/Scripts/PlatformSpawner.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject platformPrefab;
+    public RectTransform[] objects;
 
     private void Start()
     {
         StartCoroutine(CubeSpawnCoroutine());
+
+
+        
     }
 
     IEnumerator CubeSpawnCoroutine()
@@ -22,7 +26,17 @@ public class PlatformSpawner : MonoBehaviour
 
     public void SpawnCube()
     {
-        Vector3 position = new Vector3(Random.Range(-2f, 2f), -10, 0);
-        Instantiate(platformPrefab, position, Quaternion.identity, gameObject.transform);   
+        int randomSelector = Random.Range(0, objects.Length);
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (randomSelector != i)
+            {
+                Vector3 spawnPos = Camera.main.ViewportToWorldPoint(objects[i].position);
+                Vector3 lastPos = Camera.main.WorldToViewportPoint(spawnPos);
+
+                Instantiate(platformPrefab, new Vector3(lastPos.x, lastPos.y, 0), Quaternion.identity);
+            }
+        }
     }
 }
